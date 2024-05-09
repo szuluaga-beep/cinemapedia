172f4c13f30c928f27f26d34e560e87d788ad815
szuluaga-beep <steven000069@hotmail.com>
2024-05-09 18:55:37 -0500

    instalación de riverpod y crear instancia del repositorio

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,15 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
