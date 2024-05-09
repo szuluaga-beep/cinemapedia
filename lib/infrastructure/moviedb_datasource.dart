b5b185b9bfc1e34b9a11d390bcbedaa8944df8b3
szuluaga-beep <steven000069@hotmail.com>
2024-05-09 17:22:09 -0500

    agregando dio y datasource obteniendo peliculas en cines

---
+++ lib/infrastructure/moviedb_datasource.dart
@@ -0,0 +1,21 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:dio/dio.dart';
+
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+class MovieDbDatasource extends MovieDatasource {
+  final dio = Dio(BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api-key': Environment.movieDbKey,
+        'language': 'es-MX'
+      }));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('/movies/now_playing');
+    final List<Movie> movies = [];
+    return movies;
+  }
+}
