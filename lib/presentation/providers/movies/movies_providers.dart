e0284881e18aa60cb3a6098252ea6ddde5b9613f
szuluaga-beep <steven000069@hotmail.com>
2024-05-10 15:54:34 -0500

    evitando solicitudes simultaneas

--- lib/presentation/providers/movies/movies_providers.dart
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -4,9 +4,8 @@ import 'package:cinemapedia/domain/entities/movie.dart';
 
 final nowPlayMoviesProvider =
     StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
-
   final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
-  
+
   return MoviesNotifier(fetchMoreMovies: fetchMoreMovies);
 });
 
@@ -14,16 +13,23 @@ typedef MoviesCallBack = Future<List<Movie>> Function({int page});
 
 class MoviesNotifier extends StateNotifier<List<Movie>> {
   int currentPage = 0;
+  bool isLoading = false;
   MoviesCallBack fetchMoreMovies;
 
   MoviesNotifier({required this.fetchMoreMovies}) : super([]);
 
   Future<void> loadNextPage() async {
+    if (isLoading) return;
+    isLoading = true;
+
     currentPage++;
 
     final List<Movie> movies = await fetchMoreMovies(page: currentPage);
 
     state = [...state, ...movies];
+
+    await Future.delayed(const Duration(milliseconds: 300));
+    isLoading = false;
     // final List<Movie> movies=
   }
 }
