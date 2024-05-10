59322613c51af28882abf804710bf55fcd01cf4f
szuluaga-beep <steven000069@hotmail.com>
2024-05-10 11:15:51 -0500

    creando carrusel de peliculas

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
@@ -40,17 +39,7 @@ class _HomeViewState extends ConsumerState<_HomeView> {
     return Column(
       children: [
         const CustomAppbar(),
-        Expanded(
-            child: ListView.builder(
-          itemCount: nowPlayingMovies.length,
-          itemBuilder: (context, index) {
-            final movie = nowPlayingMovies[index];
-
-            return ListTile(
-              title: Text(movie.title),
-            );
-          },
-        ))
+        MoviesSlideShow(movies: nowPlayingMovies)
       ],
     );
   }
---
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -0,0 +1,40 @@
+import 'package:card_swiper/card_swiper.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:flutter/material.dart';
+
+class MoviesSlideShow extends StatelessWidget {
+  final List<Movie> movies;
+
+  const MoviesSlideShow({super.key, required this.movies});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 210,
+      width: double.infinity,
+      child: Swiper(
+        viewportFraction: 0.8,
+        scale: 0.9,
+        autoplay: true,
+        itemCount: movies.length,
+        itemBuilder: (context, index) {
+          return _Slide(
+            movie: movies[index],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+  const _Slide({
+    required this.movie,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
