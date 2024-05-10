932e332156bcecbbd8610b2dd0c0c672fa86227f
szuluaga-beep <steven000069@hotmail.com>
2024-05-10 11:58:28 -0500

    creando la paginación e instalando animate_do

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
@@ -9,11 +10,17 @@ class MoviesSlideShow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     return SizedBox(
       height: 210,
       width: double.infinity,
       child: Swiper(
         viewportFraction: 0.8,
+        pagination: SwiperPagination(
+            margin: const EdgeInsets.only(top: 0),
+            builder: DotSwiperPaginationBuilder(
+                activeColor: colors.primary, color: colors.secondary)),
         scale: 0.9,
         autoplay: true,
         itemCount: movies.length,
@@ -35,6 +42,29 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final decoration = BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+              color: Colors.black45, blurRadius: 10, offset: Offset(0, 10))
+        ]);
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+          decoration: decoration,
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.backdropPath,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return const DecoratedBox(
+                        decoration: BoxDecoration(color: Colors.black38));
+                  }
+                  return FadeIn(child: child);
+                },
+              ))),
+    );
   }
 }
