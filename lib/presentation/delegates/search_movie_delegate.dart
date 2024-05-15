903d26ea39c462bf39ea6c773d1ccb1c63cf8026
szuluaga-beep <steven000069@hotmail.com>
2024-05-15 18:35:09 -0500

    leading y actions

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,18 +1,26 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
-  
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   String get searchFieldLabel => 'Buscar película';
 
   @override
   List<Widget>? buildActions(Object context) {
-    return [const Text('BuldActions')];
+    return [
+      if (query.isNotEmpty)
+        FadeIn(
+            child: IconButton(
+                onPressed: () => query = '', icon: const Icon(Icons.clear)))
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('buildLeading');
+    return IconButton(
+        onPressed: () => close(context, null),
+        icon: const Icon(Icons.arrow_back_ios_new_rounded));
   }
 
   @override
