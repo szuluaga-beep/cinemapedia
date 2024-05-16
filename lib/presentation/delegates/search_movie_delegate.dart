2dcb60b923b26cf287497cd7213b9e3aeffe5539
szuluaga-beep <steven000069@hotmail.com>
2024-05-15 19:21:10 -0500

    mostrar las peliculas

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -45,12 +45,57 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
           itemCount: movies.length,
           itemBuilder: (context, index) {
             final movie = movies[index];
-            return ListTile(
-              title: Text(movie.title),
-            );
+            return _MovieItem(movie: movie);
           },
         );
       },
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+  const _MovieItem({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        children: [
+          SizedBox(
+            width: size.width * 0.2,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  movie.posterPath,
+                  loadingBuilder: (context, child, loadingProgress) =>
+                      FadeIn(child: child),
+                )),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          SizedBox(
+            width: size.width * 0.7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  movie.title,
+                  style: textStyles.titleMedium,
+                ),
+                (movie.overview.length > 100)
+                    ? Text('${movie.overview.substring(0, 100)}...')
+                    : Text(movie.overview)
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
