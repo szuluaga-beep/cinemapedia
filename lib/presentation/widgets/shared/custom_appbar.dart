442ab63255e46c28a250a2437d6e1aaef3de6825
szuluaga-beep <steven000069@hotmail.com>
2024-05-15 19:38:38 -0500

    realizar la busqueda con argumentos

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,7 +1,9 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
@@ -32,10 +34,14 @@ class CustomAppbar extends ConsumerWidget {
                     onPressed: () {
                       final movieRepository = ref.read(movieRepositoryProvider);
 
-                      showSearch(
-                          context: context,
-                          delegate: SearchMovieDelegate(
-                              searchMovie: movieRepository.searchMovie));
+                      showSearch<Movie?>(
+                              context: context,
+                              delegate: SearchMovieDelegate(
+                                  searchMovie: movieRepository.searchMovie))
+                          .then((movie) {
+                        if (movie == null) return;
+                        context.push('/movie/${movie.id}');
+                      });
                     },
                     icon: const Icon(Icons.search_outlined))
               ],
