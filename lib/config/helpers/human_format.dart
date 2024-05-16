0f1083abee008a34ce10e70dbdfcbed3589babd7
szuluaga-beep <steven000069@hotmail.com>
2024-05-15 19:26:48 -0500

    mostrar calificación de la pelicula

--- lib/config/helpers/human_format.dart
+++ lib/config/helpers/human_format.dart
@@ -1,10 +1,10 @@
 import 'package:intl/intl.dart';
 
 class HumanFormats {
-  static String number(double number) {
-    final formattedNumber =
-        NumberFormat.compactCurrency(decimalDigits: 1, symbol: '', locale: 'en')
-            .format(number);
+  static String number(double number, [int decimals = 0]) {
+    final formattedNumber = NumberFormat.compactCurrency(
+            decimalDigits: decimals, symbol: '', locale: 'en')
+        .format(number);
     return formattedNumber;
   }
 }
--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_format.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -69,7 +70,7 @@ class _MovieItem extends StatelessWidget {
           SizedBox(
             width: size.width * 0.2,
             child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.circular(10),
                 child: Image.network(
                   movie.posterPath,
                   loadingBuilder: (context, child, loadingProgress) =>
@@ -90,7 +91,19 @@ class _MovieItem extends StatelessWidget {
                 ),
                 (movie.overview.length > 100)
                     ? Text('${movie.overview.substring(0, 100)}...')
-                    : Text(movie.overview)
+                    : Text(movie.overview),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star_half_rounded,
+                      color: Colors.yellow.shade800,
+                    ),
+                    Text(
+                      HumanFormats.number(movie.voteAverage, 1),
+                      style: TextStyle(color: Colors.yellow.shade900),
+                    )
+                  ],
+                )
               ],
             ),
           )
